e57072801e216c4fb3406bfa24c241037c7d6056
ImxYJL <allensain14@gmail.com>
2023-07-06 16:30:52 +0900

    conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,25 @@ function Expenses(props){
         setSelectedYear(selectedYear);
     }
 
+    const selectedYears = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === selectedYear;
+    });
+    //내 코드
+    //const selectedYears = props.items.filter((expense) => (expense.date.getFullYear() == selectedYear));
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(selectedYears.length > 0){
+        expensesContent = selectedYears.map((expense) => (
+            <ExpenseItem 
+                key = {expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+            />
+        ));
+    }
+
     return(
         <div>
             <Card className="expenses">
@@ -19,10 +38,12 @@ function Expenses(props){
                     onChangeFilter={filterChangeHandler}
                     selected={selectedYear}
                 />
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
+                {expensesContent}
+                    
+                {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
                 <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
+                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/> */}
             </Card>
         </div>
     );
--- src/components/NexExpense/NewExpense.js
+++ src/components/NexExpense/NewExpense.js
@@ -1,19 +1,36 @@
-//import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [displayForm, setDisplayForm] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString() //임시 id를 추가함
         };
         props.onAddExpense(expenseData); //to App.js
+        setDisplayForm(false);
+    };
+
+    const startEditingHandler = (e) => {
+        setDisplayForm(true);
+    };
+
+    const cancelHandler = (e) => {
+        setDisplayForm(false);
     };
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/> 
+            {!displayForm && 
+                (<button onClick={startEditingHandler}>Add New Expense</button>)}
+                {/* 주의: () 없으면 렌더링 안됨 */}
+            {displayForm && 
+                (<ExpenseForm 
+                    onSaveExpenseData={saveExpenseDataHandler} 
+                    onCancel={cancelHandler}/>) }
         </div>
     );
 };
